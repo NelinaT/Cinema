84efd940b9be397e3527513cf38741dcca2a568a
Nelina Toteva <nelinatoteva@gmail.com>
2024-01-23 18:56:22 +0200

    seats view

---
+++ app/hall_builder.py
@@ -0,0 +1,37 @@
+import turtle
+import tkinter
+from svg_turtle import SvgTurtle
+
+t1 = turtle.Turtle(shape='circle')
+
+t = turtle.Turtle()
+t.hideturtle()
+
+for i in range(0,1):
+    for j in range(0,1):
+        t.goto(i*100,j*100)
+        t.pendown()
+        t.pen(pencolor="black", fillcolor="green", pensize= 5, speed=99900)
+        print(t.pos())
+        t.begin_fill()
+        t.write("55", align="center", font=('Arial', 20, 'normal'))
+        t.circle(30)
+        t.end_fill()
+        t.penup()
+
+t.pen(fillcolor = "blue", pensize= 2)
+t.begin_fill()
+t.goto(-60,-60)
+t.forward(500) 
+t.left(90)
+t.forward(50) # Forward turtle by s units
+t.left(90) # Turn turtle by 90 degree
+t.forward(500) # Forward turtle by s units
+t.left(90) # Turn turtle by 90 degree
+t.forward(50) # Forward turtle by s units
+t.left(90) # Turn turtle by 90 degree
+t.end_fill()
+
+
+
+turtle.done()
